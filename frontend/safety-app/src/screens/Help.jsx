03c9a477c7c59585a66d11aed2421a8a72fe432c
Shriti Negi <shritinegi24@gmail.com>
2025-11-24 18:56:00 +0530

    modified help.jsx

--- frontend/safety-app/src/screens/Help.jsx
+++ frontend/safety-app/src/screens/Help.jsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
+import React,{useState} from 'react'
+import ModalComp from '../components/ModalComp.js'
+import CardComp from '../components/CardComp.js'
+import { PaperProvider } from 'react-native-paper'
+import { FontAwesome, Ionicons, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Help = () => {
+
+
+const Help = ({navigation}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+  
+
+
+
+
   return (
-    <View>
-      <Text>Help</Text>
-    </View>
+    <>
+
+    <PaperProvider>
+      <View style={styles.container}>
+      <CardComp title="Video Recording" onPress={showModal} />
+        
+      </View>
+    </PaperProvider>
+    </>
+   
   )
 }
 
 export default Help
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+  
